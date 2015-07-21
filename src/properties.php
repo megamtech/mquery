3906e85a420c5bf2881c41a07bffbd812ad7df63
balajiv <balajiv@localhost>
2015-07-21 08:08:42 +0000

    Wo Invoice Pro Installed ..

--- src/properties.php
+++ src/properties.php
@@ -1,15 +1,16 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-define('AppLoggerModule', './../' . 'mlogger/trunk/src/');
+define('AppLoggerModule', './../' . 'mlogger/src/');
 
 //define('AppLogPath', AppRoot.'/logs/'.ProductName.'.log');
 //define('AppLogLevel', 2);
 //define('AppLogDateFormat', 'Y-m-d H:i:s');
 //define('AppLoggerName', 'main');
+
 ?>
